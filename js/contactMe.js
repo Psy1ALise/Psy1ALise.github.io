4695aa631c849c7c92468a864a264f0794e3bfbe
Psy1ALise <54691057+Psy1ALise@users.noreply.github.com>
2021-10-02 22:18:55 -0400

    js

--- js/contactMe.js
+++ js/contactMe.js
@@ -1,66 +1,20 @@
-function emailIsValid (email) {
-    return /\S+@\S+\.\S+/.test(email);
-}
+const btn = document.getElementById('button');
 
-function phoneIsValid (phone) {
-    return /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(phone);
-}
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
 
-function usrForm(){
-    let usrLName = document.getElementByld("lastname").value;
-    let usrFName = document.getElementByld("fastname").value;
-    let usrEmail = document.getElementById("email").value;
-    let usrPhone = document.getElementById("phone").value;
-    let usrMsg = document.getElementById("msg").value;
-    var parameters = {usrLName, usrLName, usrEmail, usrPhone, usrMsg};
-    console.log(usrLName);
-    console.log(usrFName);
-    console.log(usrEmail);
-    console.log(usrPhone);
-    console.log(usrMsg);
+   btn.value = 'Sending...';
 
-    var ready = false;
+   const serviceID = 'default_service';
+   const templateID = 'template_agodqhj';
 
-    if(usrLName == ""){
-        ready = false;
-        document.getElementById("lastNamePrompt").style.display = "";
-    }else{
-        ready = true;
-        document.getElementById("lastNamePrompt").style.display = "none";
-    }
-    if(usrFName == ""){
-        ready = false;
-        document.getElementById("firstNamePrompt").style.display = "";
-    }else{
-        ready = true;
-        document.getElementById("fastNamePrompt").style.display = "none";
-    }
-    if(!emailIsValid(usrEmail)){
-        ready = false;
-        document.getElementById("emailPrompt").style.display = "";
-    }else{
-        ready = true;
-        document.getElementById("emailPrompt").style.display = "none";
-    }
-    if(!phoneIsValid(usrPhone)){
-        ready = false;
-        document.getElementById("phonePrompt").style.display = "";
-    }else{
-        ready = true;
-        document.getElementById("phonePrompt").style.display = "none";
-    }
-    if(usrMsg == ""){
-        ready = false;
-        document.getElementById("messagePrompt").style.display = "";
-    }else{
-        ready = true;
-        document.getElementById("messagePrompt").style.display = "none";
-    }
-
-    if(ready){
-        emailjs.send("service_g0acgqq","template_agodqhj",parameters)
-            .then(function(res){
-                console.log("success",res.status);
-            });
-    }
-}
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Submit';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Submit';
+      alert(JSON.stringify(err));
+    });
+});
